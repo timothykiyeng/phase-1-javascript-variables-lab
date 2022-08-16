41357d6f78a263011e5594a6508ea5edc9c41daa
Timothy Kiyeng <timothy.kiyeng@student.moringaschool.com>
2022-08-16 08:45:38 +0300

    Completed assigment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Code your solution in this file!
+const companyName = "Scuber";
+
+//const kompanyName = "Scuber";
+
+let mostProfitableNeighborhood = "Chelsea";
+
+let companyCeo = "Susan Smith";
